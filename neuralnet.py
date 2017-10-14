e9f6913ee2109a24eab041fd3eb3ab9c847cb1c1
Saejin Mahlau-Heinert <saejinmh@gmail.com>
2017-10-13 20:39:52 -0400

    neuralnet.py completed, next up is genetic algorithms and loading/saving weight data for each network

--- neuralnet.py
+++ neuralnet.py
@@ -4,61 +4,54 @@ import numpy as np
 
 class NeuralNetwork:
     
-    def __init__(self, layerSize):
+    def __init__(self, layerSize, weights=None):
         self.layerCount = len(layerSize) - 1
         self.shape = layerSize
-        self.weights = []
         
         self._layerInput = []
         self._layerOutput = []
         
-        #create weight matrixes
-        for (inp, out) in zip(layerSize[:-1], layerSize[1:]):
-            self.weights.append(np.random.normal(scale=1, size=(out, inp+1)))
-            
-        #
+        if weights == None:
+            self.weights = []
+            #create weight matrixes
+            for (inp, out) in zip(layerSize[:-1], layerSize[1:]):
+                self.weights.append(np.random.normal(scale=0.2, size=(out, inp+1)))
+            #
+        else:
+            self.weights = weights
     #
     
-    def propagate(self, input):
-        # assume input is in form [[x1, y1, z1],[x2,y2,z2]] where each row should be transposed into column matrix/vector for propagation, and multiple rows in the input map to different vectors for parallel input running.
-        
-        numCases = input.shape[0]
-        
+    def evaluate(self, input):
+        # assume input is in form [x, y, z, ...] or np.array([x, y, z, ...]) or [[x, y, z, ...]]
+        input = np.array(input, ndmin=2)
         #clear out previous layer in/out lists
         self._layerInput = []
         self._layerOutput = []
         
         for index in range(self.layerCount):
             if index == 0:
-                #transpose to columns, add 1 to bottom, so matrix will look like below with numCases columns
-                # x1 x2 ...
-                # y1 y2 ...
-                # z1 z2 ...
-                #  1  1 ...
-                matIn = np.vstack([input.T, np.ones([1, numCases])])
-                layerInput = self.weights[0].dot(matIn)
+                #transpose to columns, add 1 to bottom, so vector will
+                #look like: x y z ... 1 vertically
+                vecIn = np.vstack((input.T, [1]))
+                layerInput = self.weights[0].dot(vecIn)
             else:
-                #take last layer output and do the same thing as done with matFirstInput
-                matIn = np.vstack([self._layerOutput[-1], np.ones([1, numCases])])
-                layerInput = self.weights[index].dot(matIn)
+                #take last layer output and do the same thing as done with before
+                vecIn = np.vstack((self._layerOutput[-1], [1]))
+                layerInput = self.weights[index].dot(vecIn)
             
             #
-            #print("before relu: " + str(layerInput))
-            #print("relu: " + str(relu(layerInput)))
-            #print("after relu: " + str(layerInput))
             
             self._layerInput.append(layerInput)
-            #self._layerOutput.append(relu(layerInput))
             
             #do softmax for last step
             if index < self.layerCount-1:
                 self._layerOutput.append(relu(layerInput))
             else:
-                #act = np.frompyfunc(softmax, 1, 1);
-                self._layerOutput.append(sigmoid(layerInput))
+                self._layerOutput.append(softmax(layerInput.T).T)
         #
         
-        return self._layerOutput[-1].T
+        #return as a numpy array, but only the output column formatted as a row
+        return self._layerOutput[-1].T[0]
         
     #
     
@@ -69,8 +62,8 @@ def relu(x):
     return np.maximum(x, 0)
     #return x if x > 0 else 0
 # relu
-def softmax(w, t=1.0):
-    e = np.exp(np.array(x) / t)
+def softmax(x, t=1.0):
+    e = np.exp(x / t)
     return e / np.sum(e)
 #
 def sigmoid(x):
@@ -80,11 +73,15 @@ def sigmoid(x):
 
 # if run as script, exec test
 if __name__ == "__main__":
-    nn = NeuralNetwork((5, 8, 8, 5))
-    print(nn.shape)
-    print(nn.weights)
+    nn = NeuralNetwork((3, 3, 2))
+    print("SHAPE: " + str(nn.shape))
+    wshape = []
+    for arr in nn.weights:
+        wshape.append(arr.shape)
+    #
+    print("WEIGHTS SHAPE: " + str(wshape) + "\nWEIGHTS: " + str(nn.weights))
     
-    input = np.array([[0, 0, 0, 0, 0], [0, 1, 0, 1, 0], [1, 1, 0, 0, 1]])
-    output = nn.propagate(input)
+    input = [0, 1, 2]
+    output = nn.evaluate(input).tolist()
     
-    print("\nInputL {0}\nOutput: {1}".format(input, output))
+    print("\nInput: " + str(input) + "\nOutput: " + str(output))
